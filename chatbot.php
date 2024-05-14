f53678f7b38813c8544fd8f92af99aa4a4c75eb5
AxelM <alex.starkpc@outlook.fr>
2024-05-14 12:16:17 +0200

    Update chatbot.php

--- chatbot.php
+++ chatbot.php
@@ -3,7 +3,10 @@
     <body>
         <iframe 
             src="https://copilotstudio.microsoft.com/environments/Default-c371d4f5-b34f-4b06-9e66-517fed904220/bots/cr4c5_esgiProjetAnnuel1/webchat?__version__=2" 
-            frameborder="0" style="width: 100%; height: 100%;">
+            frameborder="0" style="width: 100%; height: 100vh;">
         </iframe>
     </body>
 </html>
+
+
+

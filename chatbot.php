4d7ce7e11bdd5ccb37636cf7f3c9d17841a9d746
AxelM <alex.starkpc@outlook.fr>
2024-05-27 10:50:17 +0200

    Chatbot edits

--- chatbot.php
+++ chatbot.php
@@ -48,43 +48,22 @@
    <body>
      <div>
        <div id="banner">
-         <h1>Contoso copilot name</h1>
+         <h1>RideAway Agent</h1>
        </div>
        <div id="webchat" role="main"></div>
      </div>
 
-     <!--
-       In this sample, the latest version of Web Chat is being used.
-       In production environment, the version number should be pinned and version bump should be done frequently.
-
-       Please visit https://github.com/microsoft/BotFramework-WebChat/tree/main/CHANGELOG.md for changelog.
-     -->
      <script crossorigin="anonymous" src="https://cdn.botframework.com/botframework-webchat/latest/webchat.js"></script>
 
      <script>
        (async function () {
-         // Specifies style options to customize the Web Chat canvas.
-         // Please visit https://microsoft.github.io/BotFramework-WebChat for customization samples.
          const styleOptions = {
-           // Hide upload button.
            hideUploadButton: true
          };
 
-         // Specifies the token endpoint URL.
-         // To get this value, visit Copilot Studio > Settings > Channels > Mobile app page.
          const tokenEndpointURL = new URL('https://defaultc371d4f5b34f4b069e66517fed9042.20.environment.api.powerplatform.com/powervirtualagents/botsbyschema/cr4c5_esgiProjetAnnuel1/directline/token?api-version=2022-03-01-preview');
 
-         // Specifies the language the copilot and Web Chat should display in:
-         // - (Recommended) To match the page language, set it to document.documentElement.lang
-         // - To use current user language, set it to navigator.language with a fallback language
-         // - To use another language, set it to supported Unicode locale
-
-         // Setting page language is highly recommended.
-         // When page language is set, browsers will use native font for the respective language.
-
-         const locale = document.documentElement.lang || 'en'; // Uses language specified in <html> element and fallback to English (United States).
-         // const locale = navigator.language || 'ja-JP'; // Uses user preferred language and fallback to Japanese.
-         // const locale = 'zh-HAnt'; // Always use Chinese (Traditional).
+         const locale = document.documentElement.lang || 'fr-FR'; 
 
          const apiVersion = tokenEndpointURL.searchParams.get('api-version');
 
@@ -109,15 +88,7 @@
              .then(({ token }) => token)
          ]);
 
-         // The "token" variable is the credentials for accessing the current conversation.
-         // To maintain conversation across page navigation, save and reuse the token.
-
-         // The token could have access to sensitive information about the user.
-         // It must be treated like user password.
-
          const directLine = WebChat.createDirectLine({ domain: new URL('v3/directline', directLineURL), token });
-
-         // Sends "startConversation" event when the connection is established.
 
          const subscription = directLine.connectionStatus$.subscribe({
            next(value) {
--- index.php
+++ index.php
@@ -42,10 +42,10 @@
 
         </div>
 
-    <!-- <iframe id="chatbot"  -->
-            <!-- src="https://copilotstudio.microsoft.com/environments/Default-c371d4f5-b34f-4b06-9e66-517fed904220/bots/cr4c5_esgiProjetAnnuel1/webchat?__version__=2"  -->
-            <!-- frameborder="0"> -->
-    <!-- </iframe> -->
+    <iframe id="chatbot" 
+            src="https://copilotstudio.microsoft.com/environments/Default-c371d4f5-b34f-4b06-9e66-517fed904220/bots/cr4c5_esgiProjetAnnuel1/webchat?__version__=2"
+            frameborder="0">
+    </iframe>
 
 </body>
 </html>

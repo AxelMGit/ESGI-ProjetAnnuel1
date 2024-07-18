5f38728a8bbbfb68ebf3cd3327827f64e461a93f
LeoPaull23 <148349639+LeoPaull23@users.noreply.github.com>
2024-07-18 19:13:58 +0200

    debut
    debut de changement

--- garage.php
+++ garage.php
@@ -35,14 +35,38 @@
 
         <div class="contener_mid">
             <dialog id="maBoiteDeDialogue" class="classdialoguegarage">
-
+                <div class="dialog_gauche">
                     <form method="post" >
                         
+                        <?php
+                            $requete = 'SELECT id_moto, marque, modele, annee 
+                                FROM motos order by marque ASC;';
+                            $ex_requete = $pdo->prepare($requete);
+                            $ex_requete->execute();
+                            $res_requete = $ex_requete->fetchAll();
+                        ?>
+                        <label >Veuillez choisir votre moto :</label>
+                        <select id="moto" name="moto">
+                            <option value="" hidden selected disabled required></option>
+                            <?php
+                            foreach ($res_requete as $valeur) {
+                                echo '<option value="' . $valeur['id_moto'] . '">' . $valeur['marque'] . ' ' . $valeur['modele'] .  '  ' . $valeur['annee'] .'</option>';
+                            }
+                            ; 
 
+                            ?>
+                        </select>
+
+                        <label> Veuillez saisir le kilometrage de la moto :</label>
+                        <input type="text" placeholder="------" name="kmtotale">
+                        
                         <button type="submit">Valider</button>
                         
-
                     </form>  
+                </div>
+                <div class="dialog_droite">
+                    
+                </div>
             </dialog>
         </div>
 

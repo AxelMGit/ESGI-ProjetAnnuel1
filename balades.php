10ce75073de68ba7607c52f8acf554d18ad46da6
AxelM <alex.starkpc@outlook.fr>
2024-07-23 08:50:57 +0200

    Commit

--- balades.php
+++ balades.php
@@ -18,23 +18,22 @@ if (!isset($_SESSION['id_user'])) {
 <body>
     <?php include('navbar.php');?>
     
-        <div class="contener_top">
-            <a href="index.php">
-                <img src="img/logo.png" alt="logo">
-            </a>
-        </div>
+    <div class="contener_top">
+        <a href="index.php">
+            <img src="img/logo.png" alt="logo">
+        </a>
+    </div>
 
-        <div style="    display: flex; justify-content: center; align-items: center;"class="contener_mid">
-            <div class="map_container">
-                <iframe class="frame_map" src="https://www.google.com/maps/d/u/0/embed?mid=1uW1ypxOXaeoHi1AmwdCquGKXmWrspcM&ehbc=2E312F&noprof=1" width="400" height="300"></iframe>
-                <iframe class="frame_map" src="https://www.google.com/maps/d/u/0/embed?mid=1uW1ypxOXaeoHi1AmwdCquGKXmWrspcM&ehbc=2E312F&noprof=1" width="400" height="300"></iframe>
-                <iframe class="frame_map" src="https://www.google.com/maps/d/u/0/embed?mid=1uW1ypxOXaeoHi1AmwdCquGKXmWrspcM&ehbc=2E312F&noprof=1" width="400" height="300"></iframe>
-            </div>
+    <div style="display: flex; justify-content: center; align-items: center;"class="contener_mid">
+        <div class="map_container">
+            <iframe class="frame_map" src="https://www.google.com/maps/d/u/0/embed?mid=1uW1ypxOXaeoHi1AmwdCquGKXmWrspcM&ehbc=2E312F&noprof=1" width="400" height="300"></iframe>
+            <iframe class="frame_map" src="https://www.google.com/maps/d/u/0/embed?mid=1uW1ypxOXaeoHi1AmwdCquGKXmWrspcM&ehbc=2E312F&noprof=1" width="400" height="300"></iframe>
+            <iframe class="frame_map" src="https://www.google.com/maps/d/u/0/embed?mid=1uW1ypxOXaeoHi1AmwdCquGKXmWrspcM&ehbc=2E312F&noprof=1" width="400" height="300"></iframe>
         </div>
+    </div>
 
-        <div class="contener_bottom">
-            <?php include('footer.php'); ?>
-        </div>
+    <div class="contener_bottom">
+        <?php include('footer.php'); ?>
     </div>
 </body>
 </html>
